32abbbb00dde822cd58536e76f0e0b9e6957ca7a
Mathieu Lorber <mathieu.lorber@gmail.com>
2016-10-08 17:19:14 +0200

    SAVEPOINT

---
+++ src/main/java/com/example/Response.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class Response {
+
+    public String text;
+
+    public Response(String text) {
+        this.text = text;
+    }
+}
--- src/main/java/com/example/TestEndpoint.java
+++ src/main/java/com/example/TestEndpoint.java
@@ -1,15 +1,34 @@
 package com.example;
 
-import org.springframework.stereotype.Service;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
 
-@Service
+@RestController
 public class TestEndpoint {
 
     @PostConstruct
     public void init() {
         System.out.println("salut ça va ?");
+    }
+
+    @RequestMapping("/salut")
+    public String salut() {
+        return "ça va ?";
+    }
+
+    @RequestMapping(value = "/p", method = RequestMethod.POST)
+    public String p () {
+        return "c good";
+    }
+
+    @RequestMapping(value = "/push", method = RequestMethod.POST)
+    public ResponseEntity<Response> push(@RequestBody Item item) {
+        return ResponseEntity.ok(new Response("Handled : " + item.input));
     }
 
 }
