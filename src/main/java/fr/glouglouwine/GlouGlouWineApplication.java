f0ae5093f61c08779d038d2e3e6bab73ddf4d5cb
Mathieu Lorber <mathieu.lorber@gmail.com>
2016-12-09 20:33:50 +0100

    WIPE SAVEPOINT

--- src/main/java/fr/glouglouwine/GlouGlouWineApplication.java
+++ src/main/java/fr/glouglouwine/GlouGlouWineApplication.java
@@ -1,18 +1,14 @@
 package fr.glouglouwine;
 
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
 import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
-import java.time.format.DateTimeFormatter;
 
 @SpringBootApplication
 public class GlouGlouWineApplication {
@@ -41,11 +37,4 @@ public class GlouGlouWineApplication {
         return edb.build();
     }
 
-    @Bean
-    public Jackson2ObjectMapperBuilder jacksonBuilder() {
-        Jackson2ObjectMapperBuilder b = new Jackson2ObjectMapperBuilder();
-        b.serializers(new LocalDateTimeSerializer(DateTimeFormatter.ISO_DATE_TIME));
-        b.deserializers(new LocalDateTimeDeserializer(DateTimeFormatter.ISO_DATE_TIME));
-        return b;
-    }
 }
