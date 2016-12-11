92079dcb45f99335f5c7f7aabfdcadec21a36532
Mathieu Lorber <mathieu.lorber@gmail.com>
2016-12-11 19:50:19 +0100

    SAVEPOINT

--- src/main/java/fr/glouglouwine/GlouglouApplication.java
+++ src/main/java/fr/glouglouwine/GlouglouApplication.java
@@ -1,10 +1,14 @@
 package fr.glouglouwine;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GlouglouApplication {
+
+	private static final Logger logger = LoggerFactory.getLogger(GlouglouApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(GlouglouApplication.class, args);
