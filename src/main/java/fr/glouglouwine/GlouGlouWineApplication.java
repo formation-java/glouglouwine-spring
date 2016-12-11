bcf75b9eef8fdeb03b35ea38e246dd5f621794d8
Mathieu Lorber <mathieu.lorber@gmail.com>
2016-12-11 20:35:51 +0100

    SAVEPOINT

--- src/main/java/fr/glouglouwine/GlouGlouWineApplication.java
+++ src/main/java/fr/glouglouwine/GlouGlouWineApplication.java
@@ -7,16 +7,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 public class GlouGlouWineApplication {
 
     private static final Logger logger = LoggerFactory.getLogger(GlouGlouWineApplication.class);
 
+    static {
+        logger.info("class loading");
+    }
+
     @Autowired
     private BottleService bottleService;
 
     public static void main(String[] args) {
+        logger.info("instanciation");
         SpringApplication.run(GlouGlouWineApplication.class, args);
     }
 
+    @PostConstruct
+    public void postConstruct() {
+        logger.info("post construct");
+        bottleService.hello();
+    }
 }
