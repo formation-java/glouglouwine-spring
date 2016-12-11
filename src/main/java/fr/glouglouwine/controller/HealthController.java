8e1feec23c0a2fbf32a8b2088d2a69294c8f2f07
Mathieu Lorber <mathieu.lorber@gmail.com>
2016-12-11 20:55:29 +0100

    SAVEPOINT

--- src/main/java/fr/glouglouwine/controller/HealthController.java
+++ src/main/java/fr/glouglouwine/controller/HealthController.java
@@ -1,12 +1,12 @@
 package fr.glouglouwine.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HealthController {
 
-    @RequestMapping("/test")
+    @RequestMapping("/")
     public String healthCheck() {
         return "hello you, I'm great !";
     }
