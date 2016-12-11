973888f885ad53b204732c56374da689a53d5a80
Mathieu Lorber <mathieu.lorber@gmail.com>
2016-12-11 20:21:29 +0100

    SAVEPOINT

--- src/main/java/fr/glouglouwine/GlouGlouWineApplication.java
+++ src/main/java/fr/glouglouwine/GlouGlouWineApplication.java
@@ -1,7 +1,5 @@
 package fr.glouglouwine;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import fr.glouglouwine.service.BottleService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,13 +10,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GlouGlouWineApplication {
 
-	private static final Logger logger = LoggerFactory.getLogger(GlouGlouWineApplication.class);
+    private static final Logger logger = LoggerFactory.getLogger(GlouGlouWineApplication.class);
 
-	@Autowired
-	private BottleService bottleService;
+    @Autowired
+    private BottleService bottleService;
 
-	public static void main(String[] args) {
-		SpringApplication.run(GlouGlouWineApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GlouGlouWineApplication.class, args);
+    }
 
 }
