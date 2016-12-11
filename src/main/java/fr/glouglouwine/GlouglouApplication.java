4bf2ed702013bd60fffc31b73406d417b0dbbbda
Mathieu Lorber <mathieu.lorber@gmail.com>
2016-12-11 19:21:31 +0100

    SAVEPOINT

--- src/main/java/fr/glouglouwine/GlouglouApplication.java
+++ src/main/java/fr/glouglouwine/GlouglouApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class GlouglouApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(GlouglouApplication.class, args);
 	}
 
 }
