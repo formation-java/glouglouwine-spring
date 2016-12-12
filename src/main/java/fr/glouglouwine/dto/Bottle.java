bca47cb13b77f4a5d4953040e3c214cb2e26db00
Mathieu Lorber <mathieu.lorber@gmail.com>
2016-12-12 01:37:14 +0100

    SAVEPOINT

--- src/main/java/fr/glouglouwine/dto/Bottle.java
+++ src/main/java/fr/glouglouwine/dto/Bottle.java
@@ -1,4 +1,15 @@
 package fr.glouglouwine.dto;
 
 public class Bottle {
+
+    private Long id;
+    private String owner;
+    private 
+     "owner": "Eric",
+             "grapeType": "MERLOT",
+             "domain": "Chateau Réaut",
+             "year": 2011,
+             "quantity": 100,
+             "openingDate": "2016-10-11T15:39:20",
+             "finishingDate": null
 }
