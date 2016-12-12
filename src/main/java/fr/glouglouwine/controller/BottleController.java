5a221089c1d5ab18ac3e341e9f89c1ba1e2273ca
Mathieu Lorber <mathieu.lorber@gmail.com>
2016-12-12 19:20:54 +0100

    SAVEPOINT

---
+++ src/main/java/fr/glouglouwine/controller/BottleController.java
@@ -0,0 +1,41 @@
+package fr.glouglouwine.controller;
+
+import fr.glouglouwine.dto.Bottle;
+import fr.glouglouwine.repository.BottleHashmapRepository;
+import fr.glouglouwine.service.BottleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
+@RestController
+@RequestMapping("/bottle")
+public class BottleController {
+
+    @Autowired
+    private BottleHashmapRepository bottleRepository;
+
+    @Autowired
+    private BottleService bottleService;
+
+    @RequestMapping("/{id}")
+    public Bottle get(@PathVariable Long id) {
+        return bottleRepository.get(id);
+    }
+
+    @RequestMapping("/}")
+    public List<Bottle> getAll() {
+        return bottleService.getAll();
+    }
+
+    @RequestMapping(method = POST)
+    public void create(@RequestBody List<Bottle> bottles) {
+        bottleService.create(bottles);
+    }
+
+}
--- src/main/java/fr/glouglouwine/service/BottleService.java
+++ src/main/java/fr/glouglouwine/service/BottleService.java
@@ -1,12 +1,40 @@
 package fr.glouglouwine.service;
 
+import fr.glouglouwine.dto.Bottle;
+import fr.glouglouwine.repository.BottleHashmapRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class BottleService {
 
     private static final Logger logger = LoggerFactory.getLogger(BottleService.class);
+
+    @Autowired
+    private BottleHashmapRepository bottleRepository;
+
+    public List<Bottle> getAll() {
+        List<Long> ids = bottleRepository.getAll();
+        List<Bottle> bottles = new ArrayList<>();
+        for (Long id : ids) {
+            bottles.add(bottleRepository.get(id));
+        }
+        return bottles;
+        // More ?
+        // return bottleRepository.getAll().stream().map(bottleRepository::get).collect(Collectors.toList());
+    }
+
+    public void create(List<Bottle> bottles) {
+        for (Bottle b : bottles) {
+            bottleRepository.create(b);
+        }
+        // More ?
+        // bottles.stream().forEach(bottleRepository::create);
+    }
 
 }
