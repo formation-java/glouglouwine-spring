c66914f09f047776ca9cd202a06b03afa691f347
Mathieu Lorber <mathieu.lorber@gmail.com>
2016-12-13 19:41:14 +0100

    s1

---
+++ src/main/java/fr/glouglouwine/GlouGlouWineApplication.java
@@ -0,0 +1,17 @@
+package fr.glouglouwine;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlouGlouWineApplication {
+
+	private static final Logger logger = LoggerFactory.getLogger(GlouGlouWineApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlouGlouWineApplication.class, args);
+	}
+
+}
